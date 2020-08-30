91059591ca1e18d30e816be24f65f9fe8bc667c8
Meziane Kechabia <meziane228@gmail.com>
2020-08-30 00:04:14 -0700

    delivrable 1 & 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -17,7 +17,7 @@ INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
---Creating table of retireing people by their most recent job title
+-- Creating table of retireing people by their most recent job title
 SELECT COUNT(ut.title), ut.title
 INTO retiring_titles
 FROM unique_titles as ut
@@ -25,6 +25,16 @@ GROUP BY title
 ORDER BY COUNT(ut.title) DESC;
 
 -- Delivrable 2
-SELECT e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
---INTO mentorship_eligibilty
-FROM employees as e, dept_emp as de, titles as t
+
+SELECT DISTINCT ON (emp_no)e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date,t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (t.to_date = '9999-01-01')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
